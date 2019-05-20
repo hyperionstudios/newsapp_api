8011c29571d8a22ca8f314b10308cdb42b6c4893
Ahmad Alsodani <ahmad.alsodani@hyperionstudios.com.au>
2019-05-20 20:56:32 +0800

    just checking

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class CategoryController extends Controller
-{
-    //
+class CategoryController extends Controller{
+
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PostController extends Controller
-{
-    //
+class PostController extends Controller{
+
 }
