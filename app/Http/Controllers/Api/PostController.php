5959681744c518b6657116804e207d88c6233e61
Ahmad Alsodani <ahmad.alsodani@hyperionstudios.com.au>
2019-05-21 23:42:33 +0800

    add eager loading to post

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -19,7 +19,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::paginate( env('POSTS_PER_PAGE') );
+        $posts = Post::with(['comments' , 'author' , 'category'])->paginate( env('POSTS_PER_PAGE') );
         return new PostsResource( $posts );
     }
 
