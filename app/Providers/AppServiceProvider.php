f7b0deaede98b78310a085f17a3cca2ac523585f
Ahmad Alsodani <ahmad.alsodani@hyperionstudios.com.au>
2019-05-22 21:37:08 +0800

    fix column length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Observers\UserObserver;
 use App\User;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         User::observe( UserObserver::class );
     }
 }
